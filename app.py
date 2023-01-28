1cd6ef3098c5d1d1998e6a1815749bed4e1a4c47
Dungyy <erickmunoz13@gmail.com>
2023-01-28 02:07:54 -0600

    took off ads, and tracker

--- app.py
+++ app.py
@@ -5,12 +5,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    visitor_ip = request.remote_addr
-    visitor_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    visitor_data = f'IP: {visitor_ip}, Time: {visitor_time}'
-    #Log the visitor data to a file or database
-    with open("visitor_log.txt", "a") as f:
-        f.write(visitor_data + '\n')
+    # visitor_ip = request.remote_addr
+    # visitor_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    # visitor_data = f'IP: {visitor_ip}, Time: {visitor_time}'
+    # #Log the visitor data to a file or database
+    # with open("visitor_log.txt", "a") as f:
+    #     f.write(visitor_data + '\n')
     return render_template("index.html")
 
 if __name__ == '__main__':
