6962c9d2fc79f3f4c3b9fecc0a89777e3a6c0862
Dungyy <erickmunoz13@gmail.com>
2023-01-27 22:50:26 -0600

    minor css edits, and added tracking ;)

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from datetime import datetime
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def home():
+    visitor_ip = request.remote_addr
+    visitor_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    visitor_data = f'IP: {visitor_ip}, Time: {visitor_time}'
+    #Log the visitor data to a file or database
+    with open("visitor_log.txt", "a") as f:
+        f.write(visitor_data + '\n')
     return render_template("index.html")
-
 
 if __name__ == '__main__':
     app.run(debug=True)
